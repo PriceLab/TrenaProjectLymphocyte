4ebf547943980fd24fc90595ba9e62b29d4149af
paul-shannon <paul.thurmond.shannon@gmail.com>
2019-04-30 13:34:50 -0700

    upped versions neeed, tp and tphg38

--- inst/demos/trenaViz/tv.R
+++ inst/demos/trenaViz/tv.R
@@ -1,6 +1,6 @@
 library(TrenaViz)
 stopifnot(packageVersion("TrenaProject") >= "1.0.0")
-stopifnot(packageVersion("TrenaProjectHG38") >= "0.99.43")
+stopifnot(packageVersion("TrenaProjectHG38") >= "0.99.44")
 tv <- TrenaViz("TrenaProjectLymphocyte")
 PORT=5874
 runApp(createApp(tv, port=PORT))
